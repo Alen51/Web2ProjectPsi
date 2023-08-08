a44d38422dcfb8f3944a07e9f767ec6e0e88e03e
Alen Eminovic <alen.eminovic@hotmail.com>
2023-08-08 21:56:56 +0200

    Dto added

---
+++ Web2Project/Dto/KorisnikDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Web2Project.Dto
+{
+    public class KorisnikDto
+    {
+        public string KorisnickoIme { get; set ; }
+        public string Lozinka { get; set; }
+        public string ImeIPrezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public string Adresa { get; set; }
+        public string TipKoorisnika { get; set; }
+        public bool Verifikovan { get; set; }
+        public string Slika { get; set; }
+        public string Email { get; set; }
+        public double Novac { get; set; }
+    }
+}
---
+++ Web2Project/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Web2Project.Dto;
+using Web2Project.Models;
+
+namespace Web2Project.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        { 
+            
+            CreateMap<Korisnik,KorisnikDto>().ReverseMap();
+            CreateMap<Paket,PaketDto>().ReverseMap();
+            CreateMap<Porudzbina,PorudzbinaDto>().ReverseMap();
+        
+        }
+    }
+}
